a83d2d578620aae3f409aba2a4e17a0ec48f0334
YourFullName <andriykrav@gmail.com>
2015-02-26 11:02:34 +0200

    26.02.2015

--- students/views/students.py
+++ students/views/students.py
@@ -1,5 +1,37 @@
-from ..models import Student
+
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Views for Students
 def students_list(request):
-	students = Student.objects.all()
+	students = (
+	{'id': 1,
+	'first_name': u'Корост',
+	'last_name': u'Андрій',
+	'ticket': 2123,
+	'image': 'img/img1.png'},
+	{'id': 2,
+	'first_name': u'Заєць',
+	'last_name': u'Андрій',
+	'ticket': 2125,
+	'image': 'img/img2.png'},
+    {'id': 3,
+	'first_name': u'Подоба',
+	'last_name': u'Віталій',
+	'ticket': 2130,
+	'image': 'img/img3.png'},
+)
 	return render(request, 'students/students_list.html',
 		{'students': students})
+
+def students_add(request):
+	return HttpResponse('<h1>Student Add Form</h1>')
+
+def students_edit(request, sid):
+	return HttpResponse('<h2>Edit Student %s</h2>' % sid)
+
+def students_delete(request, sid):
+	return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
